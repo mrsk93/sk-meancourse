2d520574fd1eb1de13af3ee68e4f12e101ceb3d1
Sumit Chalotra <kumar.sumit9981@gmail.com>
2020-06-28 19:13:39 +0530

    first and final commit 2.0

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -65,7 +65,7 @@ exports.getPosts = (req, res, next) => {
   postQuery
     .then(documents => {
       fetchedPosts = documents;
-      return Post.count();
+      return Post.countDocuments();
     })
     .then(count => {
       res.status(200).json({
